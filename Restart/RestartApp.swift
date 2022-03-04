b5b98e2fea2745fb950dd799d083bc80c9913a5c
JonathasLopes <jon.athasmoreira@hotmail.com>
2022-03-03 23:47:12 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jonathas Lopes on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
